55a4fc4c960b0a39548d1d0c49c1789a9d404c20
noceive <nocivescience@gmail.com>
2022-08-12 03:16:21 -0400

    terminado

--- proof.js
+++ proof.js
@@ -1,12 +1,36 @@
-let prueba={};
-const prueba2=Object.assign(prueba,{x:5})
-const prueba3=Object.assign(prueba,{t:6,h:8});
-const lista=[
-    {x:3,y:7},
-    {x:1,y:5},
-    {x:7,y:2},
-    {x:6,y:9},
-    {x:1,y:9}
-]
-const prueba4=Object.assign(prueba,lista[lista.length-1])
-console.log(prueba)
+// let prueba={};
+// const prueba2=Object.assign(prueba,{x:5})
+// const prueba3=Object.assign(prueba,{t:6,h:8});
+// const lista=[
+//     {x:3,y:7},
+//     {x:1,y:5},
+//     {x:7,y:2},
+//     {x:6,y:9},
+//     {x:1,y:9}
+// ]
+// const prueba4=Object.assign(prueba,lista[lista.length-1])
+// console.log(prueba)
+
+// lista=[2,5,7,9,12,18,20];
+// // console.log(lista.splice(3,2))
+
+// lista=[{x:1,y:1}]
+// function randomXY(){
+//     return {
+//         x: parseInt(Math.random()*100),
+//         y: parseInt(Math.random()*100),
+//     }
+// }
+// function ejecucion(){
+//     lista.push(randomXY())
+//     console.log(lista)
+// };
+// ejecucion()
+
+let state={
+    snake:[{x:1,y:2}]
+}
+let tail={};
+let newTail=Object.assign(tail, state.snake[state.snake.length-1])
+let newTail2=state.snake.push(tail)
+console.log(newTail2)
--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-const gameEl=document.getElementById('game');
+const gameEl=document.querySelector('canvas');
 const ctx=gameEl.getContext('2d');
 gameEl.width=window.innerWidth;
 gameEl.height=window.innerHeight;
 const stateRunning=1;
+const stateLosing=2;
+const growScale=10;
 const TICK=80;
+const squareSize=10;
 const directionMap={
     'a':[-1,0],
     'd':[1,0],
-    'w':[0,1],
-    's':[0,-1]
+    'w':[0,-1],
+    's':[0,1]
 };
 let state={
     snake:[{x:0,y:0}],
@@ -27,7 +30,8 @@ window.onload=function(){
                 state.direction.y=y;
             }
         }
-    }
+    };
+    tick();
 }
 function randomXY(){
     return {
@@ -42,6 +46,77 @@ function tick(){
     const highestIndex=state.snake.length-1;
     let tail={};
     let interval=TICK;
-    Object.assign(tail,state.snake[state.snake.length-1])
+    Object.assign(tail,state.snake[state.snake.length-1]);
+    let didScore=(
+        head.x===state.prey.x&&head.x===state.prey.y&&head.y
+    )
+    if(state.runState===stateRunning){
+        for(let idx=highestIndex;idx>-1;idx--){
+            const sq=state.snake[idx];
+            if(idx===0){
+                sq.x+=dx;
+                sq.y+=dy;
+            }else{
+                sq.x=state.snake[idx-1].x;
+                sq.y=state.snake[idx-1].y;
+            }
+        }
+    }else if(state.runState===state.stateLosing){
+        interval=10;
+        if(state.snake.length>0){
+            state.snake.aplice(0,1)
+            state.snake.push(randomXY())
+            state.prey=randomXY();
+        }
+    }
+    if(detectCollision()){
+        state.runState=stateLosing;
+        state.growing=0;
+    };
+    if(didScore){
+        state.growing+=growScale;
+        state.prey=randomXY();
+    }
+    if(state.growing>0){
+        state.snake.push(tail);
+        state.growing-=1;
+    }
+    requestAnimationFrame(draw);
+    setTimeout(tick,interval)
 }
-tick();
+function detectCollision(){
+    const head=state.snake[0];
+    if(
+        head.x<0||
+        head.x>=gameEl.width||
+        head.y<0||
+        head.y>=gameEl.height
+    ){
+        return true
+    }
+    for(var idx=1;idx<state.snake.length;idx++){
+        const sq=state.snake[idx];
+        if(sq.x===head.x&&sq.y===head.y){
+            return true
+        }
+    }
+    return false
+}
+function drawPixel(color,x,y){
+    ctx.fillStyle=color;
+    ctx.fillRect(
+        x*squareSize,
+        y*squareSize,
+        squareSize,
+        squareSize
+    )
+}
+function draw(){
+    ctx.clearRect(0,0,gameEl.width,gameEl.height);
+    for(var idx=0;idx<state.snake.length;idx++){
+        const {x,y}=state.snake[idx];
+        drawPixel('red',x,y)
+    }
+    const {x,y}=state.prey;
+    drawPixel('yellow',x,y)
+}
